2daf395895f3a81cec02f49886030861f56d689f
Ramenoodle <romanja305@gmail.com>
2026-06-22 10:16:02 -0700

    "Updated file name"

---
+++ animal.py.py
@@ -0,0 +1,8 @@
+# Author: Roman Anderson
+# GitHub username: Ramenoodle
+# Date: 6/22/2026
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
